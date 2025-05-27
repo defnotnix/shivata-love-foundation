20be3c59c31dea9f0de868fd608e649a560f63cc
Anamol <anamol.maharjan.dev@gmail.com>
2025-05-27 14:09:54 +0545

    Added Update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,5 @@
 "use client";
 
-//import { PageHome } from "@/modules/home";
+import PageHome from "@/modules/home";
 
-import dynamic from "next/dynamic";
-
-const PageHome = dynamic(() => import("../../modules/home/index"), {
-  ssr: false,
-});
-
-export default function Page() {
-  return <PageHome />;
-}
+export default PageHome;
